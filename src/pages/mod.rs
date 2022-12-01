9231ff8123e43fa54dd8ce933fdb4142102eec0d
averichev <averichev@yandex.ru>
2022-12-01 15:56:09 +0300

    fix: PageRepository::add signature

--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -1,8 +1,9 @@
 use crate::pages::page::{Page, PageId};
+use crate::sites::site::SiteId;
 
 pub mod page;
 
 pub trait PageRepository {
-    fn add(page: Page) -> PageId;
+    fn add(page: Page, site_id: SiteId) -> PageId;
     fn delete(page_id: PageId) -> bool;
 }
