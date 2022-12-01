5522b98b6c2502f0332cfbba29ce902295c594f1
averichev <averichev@yandex.ru>
2022-12-01 16:04:10 +0300

    fix: PageRepository::add rename

--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -4,6 +4,6 @@ use crate::sites::site::SiteId;
 pub mod page;
 
 pub trait PageRepository {
-    fn add(page: Page, site_id: SiteId) -> PageId;
+    fn create(page: Page, site_id: SiteId) -> PageId;
     fn delete(page_id: PageId) -> bool;
 }
