0b24c5c46c77eb986b408a02dfab6c27807ab443
averichev <averichev@yandex.ru>
2022-12-02 12:16:27 +0300

    fix: SiteRepository::add rename

--- src/sites/mod.rs
+++ src/sites/mod.rs
@@ -3,6 +3,6 @@ pub mod site;
 use crate::sites::site::{Site, SiteId};
 
 pub trait SiteRepository {
-    fn add(site: Site) -> SiteId;
+    fn create(site: Site) -> SiteId;
     fn delete(site_id: SiteId) -> bool;
 }
