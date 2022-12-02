fe283e6c2c3147ac6b5f772f900751fb35157955
averichev <averichev@yandex.ru>
2022-12-02 13:21:26 +0300

    improvement: SiteIdBuilder

--- src/sites/mod.rs
+++ src/sites/mod.rs
@@ -1,6 +1,18 @@
 use url::Url;
 use crate::pages::PageId;
 
+pub trait SiteKit {
+    fn get_site(url: Url) -> (Site, SiteId);
+}
+
+pub trait SiteIdBuilder {
+    fn build(id: u8) -> SiteId {
+        SiteId {
+            value: id,
+        }
+    }
+}
+
 pub trait SiteRepository {
     fn create(site: Site) -> SiteId;
     fn delete(site_id: SiteId) -> bool;
